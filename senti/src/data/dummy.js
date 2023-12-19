a6b325face0e425824e8e743a60cb52a82f53038
sudharaathith <t.sudharaathith@gmail.com>
2023-12-19 16:39:09 +0530

    summa

--- senti/src/data/dummy.js
+++ senti/src/data/dummy.js
@@ -543,6 +543,45 @@ export const feedbacks = [
   },
 ]
 
+export const employeesData = [
+  {
+    EmployeeID: 1,
+    Name: 'Nancy Davolio',
+    Title: 'Sales Representative',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+  
+  },
+  {
+    EmployeeID: 2,
+    Name: 'Nasimiyu Danai',
+    Title: 'Marketing Head',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    
+  },
+  {
+    EmployeeID: 3,
+    Name: 'Iulia Albu',
+    Title: 'HR',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    
+  },
+  {
+    EmployeeID: 4,
+    Name: 'Siegbert Gottfried',
+    Title: 'Marketing Head',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    
+  },
+  ]
+
 
 export const chatData = [
   {
